5ede18cdf8d38debe555415313e83fa18adaafd3
hamitmizrak <hamitmizrak@gmail.com>
2021-11-07 13:59:39 +0300

    Araştırma ödevleri-2 Error(Syntax error –Logical error- runtime error nedir örnek veriniz) 	  Heap memory nedir bize ne gibi avantajları vardır ? 	  Stack memory nedir bize ne gibi avantajları vardır ?

---
+++ src/com/ecodation/a0/odevler/_B2_arastirmaOdevi.java
@@ -0,0 +1,9 @@
+package com.ecodation.a0.odevler;
+
+public class _B2_arastirmaOdevi {
+	/*
+	 * Error(Syntax error –Logical error- runtime error nedir örnek veriniz)
+	 * Heap memory nedir bize ne gibi avantajları vardır ?
+	 * Stack memory nedir bize ne gibi avantajları vardır ?
+	 */
+}
