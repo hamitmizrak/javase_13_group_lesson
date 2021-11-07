09754e7c4db231acc5ba530b4001c139e5117c99
hamitmizrak <hamitmizrak@gmail.com>
2021-11-07 13:59:22 +0300

    Araşırma ödevi-1 java nedir diğer dillerden fakı nedir? 	 interpreter nedir 	  compiler nedir 	  jdk jre jvm arasındahki farklar

---
+++ src/com/ecodation/a0/odevler/_B1_ArastirmaOdevleri.java
@@ -0,0 +1,10 @@
+package com.ecodation.a0.odevler;
+
+public class _B1_ArastirmaOdevleri {
+	/*
+	 * java nedir diğer dillerden fakı nedir?
+	 * interpreter nedir
+	 * compiler nedir
+	 * jdk jre jvm arasındahki farklar
+	 */
+}
