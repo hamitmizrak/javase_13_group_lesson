2970047dc87d7fa216e64777b87f529e446822ed
hamitmizrak <hamitmizrak@gmail.com>
2021-11-07 14:00:54 +0300

    Örnek: 4 Hesap makinası

---
+++ src/com/ecodation/a0/odevler/_A4_HesapMakinasi.java
@@ -0,0 +1,12 @@
+package com.ecodation.a0.odevler;
+
+public class _A4_HesapMakinasi {
+	// iki sayı değişkemni oluşturun
+	int sayi1 = 10, sayi2 = 5;
+	// toplamaları
+	// farkını
+	// çarpma
+	// bölmesi
+	// bölümünden kalanı
+	
+}
