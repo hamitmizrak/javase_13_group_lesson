9505003c562c8d32626ec37b260f15cb3667b5aa
hamitmizrak <hamitmizrak@gmail.com>
2021-11-07 13:47:42 +0300

    işlem sırası

---
+++ src/com/ecodation/a2/variables/_05_IslemSirasi.java
@@ -0,0 +1,11 @@
+package com.ecodation.a2.variables;
+
+public class _05_IslemSirasi {
+	public static void main(String[] args) {
+		// işlem sırasında öncelikle parantez içi yapılır
+		// bölme veya çarpma yapılır
+		// en son toplama veya çıkarma yapılır.
+		System.out.println(5 * (4 * 4 + 5) + 9 / 9);
+		
+	}
+}
