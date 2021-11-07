3dd3987ce4b335573bb98e865f2d082aaddeddf7
hamitmizrak <hamitmizrak@gmail.com>
2021-11-07 13:49:04 +0300

    sayı sistemleri

---
+++ src/com/ecodation/a2/variables/_13_sayiSistemleri.java
@@ -0,0 +1,21 @@
+package com.ecodation.a2.variables;
+
+public class _13_sayiSistemleri {
+	public static void main(String[] args) {
+		// binary: 2(0,1)
+		int binaryNumber = 0b0001110;
+		System.out.println("binary: " + binaryNumber);
+		
+		// octal: 8(0,1,3,4,5,6,7)
+		int octalNumber = 01234567;
+		System.out.println("octal: " + octalNumber);
+		
+		// hexadecimal:16 tabanada(0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F)
+		int hexadecimalNumber = 0x45AB;
+		System.out.println("hexa: " + hexadecimalNumber);
+		
+		// decimal:10 taban sistemi(0,1,2,3,4,5,6,7,8,9)
+		int decimal = 152;
+		System.out.println("decimal: " + decimal);
+	}
+}
