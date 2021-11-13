d97b9d6eaeddab9cde4d63d6245d992013c2072d
hamitmizrak <hamitmizrak@gmail.com>
2021-11-13 13:48:23 +0300

    StringTokenizer

---
+++ src/com/ecodation/a3/string/_13_StringTokenizer.java
@@ -0,0 +1,28 @@
+package com.ecodation.a3.string;
+
+import java.util.StringTokenizer;
+
+public class _13_StringTokenizer {
+	
+	public static void main(String[] args) {
+		// java.lang kütüphanesinden çağırırız
+		
+		String kelime2 = "Java,JSP,JSF,Spring,SpringBoot&&SpringData";
+		// 1.yöntem
+		String[] dizi = kelime2.split(",");
+		for (String temp : dizi) {
+			System.out.println(temp);
+		}
+		
+		System.out.println("*****************");
+		
+		// 2.yöntem
+		// eğer büyük veriler varsa şunu kullanalaım.
+		StringTokenizer stringTokenizer = new StringTokenizer(kelime2, ",&&");
+		while (stringTokenizer.hasMoreTokens()) {
+			String stok = stringTokenizer.nextToken();
+			System.out.println(stok);
+		}
+	}
+	
+}
