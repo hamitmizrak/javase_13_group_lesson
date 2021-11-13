137afeca848cf28da1525974b79bcd8888496a4f
hamitmizrak <hamitmizrak@gmail.com>
2021-11-13 12:50:42 +0300

    Escape

---
+++ src/com/ecodation/a3/string/_06_escape.java
@@ -0,0 +1,14 @@
+package com.ecodation.a3.string;
+
+public class _06_escape {
+	
+	public static void main(String[] args) {
+		// "" ''
+		// escape karakter(Çıkış karakter)
+		System.out.println("1.adım\"");
+		System.out.println("2.adım\'");
+		System.out.println("3.adım\n"); // n=yeni satıra geç
+		System.out.println("4.adım\tyeni"); // t=tab karakter boşluk bırak anlamına geliyor.
+	}
+	
+}
