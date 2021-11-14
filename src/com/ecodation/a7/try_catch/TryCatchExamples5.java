4effd9d44056fa7cc33f31b36ec188318700402e
hamitmizrak <hamitmizrak@gmail.com>
2021-11-14 14:32:29 +0300

    Try catch finally throw throws

---
+++ src/com/ecodation/a7/try_catch/TryCatchExamples5.java
@@ -0,0 +1,18 @@
+package com.ecodation.a7.try_catch;
+
+// import java.util.*; ==> util paketindeki bütün metotları çağır
+
+public class TryCatchExamples5 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("1.satır");
+		String hatameydanaGelsin = "istisna meydana gelsin";
+		System.out.println(hatameydanaGelsin);
+		// istisna zorunluluğu olsun yapay istisna oluşturmak istiyorsam
+		throw new ArithmeticException("Yanlış yaptınız");
+		// System.out.println("son satır");
+		
+	}
+	
+}
---
+++ src/com/ecodation/a7/try_catch/TryCatchExamples6.java
@@ -0,0 +1,30 @@
+package com.ecodation.a7.try_catch;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+// import java.util.*; ==> util paketindeki bütün metotları çağır
+
+public class TryCatchExamples6 {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		Connection connection = null;
+		// try -with resources
+		try {
+			System.out.println("1.satır");
+			int sayi = 4 / 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw (e);
+		} finally {
+			// finally zorunlu olarak kapatılması gereken kodları kapatır.
+			// database.close()
+			// kalvye.close();
+			// socket kapat
+			connection.close();
+		}
+		
+	}
+	
+}
