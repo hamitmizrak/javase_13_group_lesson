f3d9201c7bb472c2bd3755a5d5d66eb595973a30
hamitmizrak <hamitmizrak@gmail.com>
2021-11-14 12:33:27 +0300

    while for döngüsü

---
+++ src/com/ecodation/a6/loop/ForDongusu.java
@@ -0,0 +1,41 @@
+package com.ecodation.a6.loop;
+
+public class ForDongusu {
+	
+	public static void main(String[] args) {
+		// döngüler:
+		// System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14
+		// + 15);
+		// for (int i = 1; i <= 1000; i++) {
+		// System.out.print(i + " ");
+		// }
+		
+		// int i;
+		// for (i = 1; i <= 1000; i++) {
+		// System.out.print(i + " ");
+		// }
+		
+		// for (int i = 1; i <= 1000; i=i+1) {
+		// System.out.print(i + " ");
+		// }
+		
+		// for (int i = 1; i <= 1000; i += 1) {
+		// System.out.print(i + " ");
+		// }
+		
+		// for (int i = 0; i <= 1000; i = i + 2) {
+		// System.out.print(i + " ");
+		// }
+		
+		// for (int i = 1; i <= 1000; i = i + 2) {
+		// System.out.print(i + " ");
+		// }
+		
+		// azaltmak
+		for (int i = 10; i > 0; i = i - 1) {
+			System.out.print(i + " ");
+		}
+		
+	}
+	
+}
