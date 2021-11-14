60f5c8a615a51d725304828cdacbda56066c6916
hamitmizrak <hamitmizrak@gmail.com>
2021-11-14 14:32:51 +0300

    Kendi excepitonumuzu oluşturduk

---
+++ src/com/ecodation/examples/a00/EcodationExcepiton.java
@@ -0,0 +1,9 @@
+package com.ecodation.examples.a00;
+
+public class EcodationExcepiton extends Exception {
+	private static final long serialVersionUID = -2372984444427107944L;
+	
+	public EcodationExcepiton(String err) {
+		super(err);
+	}
+}
