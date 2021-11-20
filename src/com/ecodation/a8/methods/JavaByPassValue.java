4d627fe4482af20717c5d26a9a09f9eeeabb57f9
hamitmizrak <hamitmizrak@gmail.com>
2021-11-20 11:10:43 +0300

    Java By pass value

---
+++ src/com/ecodation/a8/methods/JavaByPassValue.java
@@ -0,0 +1,22 @@
+package com.ecodation.a8.methods;
+
+public class JavaByPassValue {
+	
+	public static void deneme(String adi) {
+		System.out.println(adi);
+	}
+	
+	public static void main(String[] args) {
+		deneme("Hamit44");
+		
+		///////// Java By Pass Value/////////////
+		// farklı belleklere kaydeder. Original adres başka belleğe yeni veri başka
+		///////// belleğe kaydeder.
+		// value: değer yapımıza göre gönderme
+		/// primitive type : Java By Pass Value
+		//
+		// Metodu gerçek değerimizi işleme almak
+		
+	}
+	
+}
